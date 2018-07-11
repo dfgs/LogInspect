1fbbd2e9cf85ecf2591e99839fccae752f348811
dfgs <dfgs83@gmail.com>
2018-07-11 18:35:49 +0200

    deleted test app

--- LogInspect/Program.cs
+++ LogInspect/Program.cs
@@ -21,35 +21,7 @@ namespace LogInspect
 
 			logger = new ConsoleLogger(new DefaultLogFormatter());
 
-			FormatHandler schema = new FormatHandler();
-			schema.Name = "RCM";
-			schema.FileNamePattern = @"^RCM\.log(\.\d+)?$";
-			schema.AppendToNextPatterns.Add(@".*(?<!\u0003)$");
-
-			rule = new LogInspectLib.Rule() { Name = "Event" };
-			rule.Tokens.Add(new Token() { Name = "Date", Pattern = @"^\d\d/\d\d/\d\d \d\d:\d\d:\d\d\.\d+" });
-			rule.Tokens.Add(new Token() { Name = null, Pattern = @" *\| *" });
-			rule.Tokens.Add(new Token() { Name = "Severity", Pattern = @"[^ ]+" });
-			rule.Tokens.Add(new Token() { Name = null, Pattern = @" *\| *" });
-			rule.Tokens.Add(new Token() { Name = "Class", Pattern = @"[^,]+" });
-			rule.Tokens.Add(new Token() { Name = null, Pattern = @", " });
-			rule.Tokens.Add(new Token() { Name = "Thread", Pattern = @"\d+" });
-			rule.Tokens.Add(new Token() { Name = null, Pattern = @" *\| *" });
-			rule.Tokens.Add(new Token() { Name = "Module", Pattern = @"[^ ]+" });
-			rule.Tokens.Add(new Token() { Name = null, Pattern = @" *\| *" });
-			rule.Tokens.Add(new Token() { Name = "Message", Pattern = @"[^\u0003]+" });
-			rule.Tokens.Add(new Token() { Name = null, Pattern = @"\u0003$" });
-			schema.Rules.Add(rule);
-
-			rule = new LogInspectLib.Rule() { Name = "Comment" };
-			rule.Tokens.Add(new Token() { Pattern = @"^//" });
-			schema.Rules.Add(rule);
-
-
-			schema.SaveToFile(@"FormatHandlers\RCM.xml");
-
-			schemaManager = new LogFileManager(logger);
-			schemaManager.LoadSchemas("FormatHandlers");
+			
 
 			LogReader reader = schemaManager.CreateLogReader(@"E:\FTP\Exemples de log\RCM.log.37176");
 
